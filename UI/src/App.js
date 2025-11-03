2190693a99a51b17777f571f9b29eb96b446ac32
rajputmanasvi <rajputmanasvi2008@gmail.com>
2025-11-03 04:35:44 +0000

    updated

--- UI/src/App.js
+++ UI/src/App.js
@@ -1,13 +1,16 @@
 import React  from 'react';
-import LeadStatus from './components/LeadStatus';
-//import ProductsTable from './components/ProductsTable';
-//import CategoriesPage from './components/Category';
-//import LeadSource from './components/LeadSource2';
+// import LeadStatus from './components/LeadStatus';
+// import ProductsTable from './components/ProductsTable';
+ import CategoriesPage from './components/Category';
+// import LeadSource from './components/LeadSource2';
 
 function App() {
   return (
     <div className="App">
-        <LeadStatus/>
+        {/* <ProductsTable/> */}
+        <CategoriesPage/>
+        {/* <LeadSource/> */}
+        {/* <LeadStatus/> */}
     </div>
   );
 }
